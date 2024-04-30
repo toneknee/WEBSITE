8a10b870f81a89d95527549bbd42f7c1e3c43799
toneknee <tony.trinh18@gmail.com>
2024-04-30 12:20:53 -0600

    Added analytics

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,6 +29,7 @@ export default function RootLayout({
         >
           <NavBar />
           {children}
+          <Analytics />
           <Footer />
         </ThemeProvider>
       </body>
